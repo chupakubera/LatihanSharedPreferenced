da8e0b9e66369ce0678376c957a1c9839d972850
chupakubera <allanggaongi@gmail.com>
2020-04-28 11:42:33 +0700

    membuat kelas UserModel

---
+++ app/src/main/java/com/chupakubera/latihansharedpreference/model/UselModel.java
@@ -0,0 +1,41 @@
+/**
+     NIM         : 10118702
+     Nama        : Erwin Jelly Barus Tobing
+     Kelas       : IF-6k
+     Tanggal     : 28/04/2020
+     Deskripsi   : membuat kelas UserModel membuat var username, password, phone dan membuat
+                   getter and setter nya
+
+ */
+
+package com.chupakubera.latihansharedpreference.model;
+
+public class UselModel {
+    private String username;
+    private String password;
+    private String phone;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
